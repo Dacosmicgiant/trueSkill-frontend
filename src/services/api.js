cdd75285eb95132225412d0561559c806a555867
yashmahamulkar <142833012+yashmahamulkar@users.noreply.github.com>
2025-05-24 11:51:30 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 // Create an Axios instance with default config
 const api = axios.create({
-  baseURL: 'https://trueskill-backend.onrender.com/', // Should match your backend URL
+  baseURL: 'https://trueskill-backend.onrender.com/api', // Should match your backend URL
   headers: {
     'Content-Type': 'application/json',
   },
